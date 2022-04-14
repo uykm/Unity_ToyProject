55d6a41098d6e2dd3cdadc4b2b0a09bb26cac492
UykM <digi1k2001@gmail.com>
2022-04-14 19:21:21 +0900

    20220414-4

--- Hello Class/Assets/Jumper.cs
+++ Hello Class/Assets/Jumper.cs
@@ -8,6 +8,14 @@ public class Jumper : MonoBehaviour
     public Rigidbody myRigidbody;       // Rigidbody - 컴포넌트(Component : 클래스 타입, 클래스로 이루어져 있음), myRigidbody - 클래스 타입의 변수( = 참조 타입)
                                         // Rigidbody 컴포넌트를 참조 타입의 myRigidbody 로 가리키고 있음.
 
+    // ★ 이해안갔던 Point!
+    // Rigidbody myRigidbody;
+    // RIgidbody myRigidbody = new Rigidbody(); 처럼 new 키워드를 사용안하는 이유
+    // : 이 Jumper 스크립트를 추가한 Cube 게임 오브젝트의 인스펙터(Inspector)에 Rigidbody 컴포넌트가 추가되어 있고,
+    // Rigidbody 컴포넌트를 Jumper 컴포넌트의 My Rigidbody 필드로 드래그&드롭했기 때문에,
+    // 참조 타입의 변수 myRigidbody 에 Cube 게임 오브젝트의 Rigidbody 컴포넌트로 향하는 참조가 할당되게 된 것 !
+    // 따라서, myRigidbody.AddForce(0, 500, 0) 처럼 참조 타입의 변수만으로 호출 가능함 !
+
     void Start()
     {
         myRigidbody.AddForce(0, 500, 0);    // 이처럼 모든 '실체' 컴포넌트는 코드 상에서 참조 타입의 변수로 가리키고 사용할 수 있음.
