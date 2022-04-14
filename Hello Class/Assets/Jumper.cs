ed6b36bd90a883679a7796b62fee74e8c3ff597c
UykM <digi1k2001@gmail.com>
2022-04-14 18:17:53 +0900

    20220414-2

--- Hello Class/Assets/Jumper.cs
+++ Hello Class/Assets/Jumper.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Jumper : MonoBehaviour
 {
-    public Rigidbody myRigidbody;
+    // 참조 타입의 변수로 컴포넌트 사용
+    public Rigidbody myRigidbody;       // Rigidbody - 컴포넌트(Component : 클래스로 이루어져 있음), myRigidbody - 클래스 타입의 변수( = 참조 타입)
+                                        // Rigidbody 컴포넌트를 참조 타입의 myRigidbody 로 가리키고 있음.
 
     void Start()
     {
-        myRigidbody.AddForce(0, 500, 0);
+        myRigidbody.AddForce(0, 500, 0);    // 이처럼 모든 '실체' 컴포넌트는 코드 상에서 참조 타입의 변수로 가리키고 사용할 수 있음.
+                                            // 변수 myRigidbody 를 사용하는 것처럼 보이지만, 사실상 myRigidbody가 가리키는 '실체' 컴포넌트 Rigidbody 가 사용되는것임!
+                                            // 따라서, Rigidbody 타입에 내장된 AddForce() 메서드 사용 가능
     }
 }
